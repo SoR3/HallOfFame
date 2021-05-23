2db3c10ac3edf942e030a93581dd1b2ba43e21ac
jystlik <jystlik@gmail.com>
2021-05-23 11:40:00 +0700

    Добавление консольного логгирования

--- HallOfFame/Controllers/PersonController.cs
+++ HallOfFame/Controllers/PersonController.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using HallOfFame.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using HallOfFame.Models;
 
 namespace HallOfFame.Controllers
 {
@@ -14,51 +14,75 @@ namespace HallOfFame.Controllers
     public class PersonController : ControllerBase
     {
         private readonly PersonContext _context;
+        private readonly ILogger<PersonController> _logger;
 
-        public PersonController(PersonContext context)
+        public PersonController(PersonContext context, ILogger<PersonController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         // GET: api/v1/person
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Person>>> GetPersons()
         {
-            var persons = await _context.Persons.Include(p => p.Skills).ToListAsync();
+            _logger.LogInformation($"api/v1/persons GET request at {DateTime.Now:hh:mm:ss}");
 
-            if(!persons.Any())
+            try
             {
-                //TODO: add logging
-                return NotFound("Not found Persons");
+                var persons = await _context.Persons.Include(p => p.Skills).ToListAsync();
+
+                if (!persons.Any())
+                {
+                    _logger.LogError("Not found persons");
+                    return NotFound("Not found Persons");
+                }
+
+                return Ok(persons);
             }
-            //TODO: add logging
-            return Ok(persons);        
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
+            
         }
 
         // GET: api/v1/person/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Person>> GetPerson(long id)
         {
-            var person = await _context.Persons.Include(p => p.Skills).FirstOrDefaultAsync(x => x.Id == id);
-
-            if (person == null)
+            _logger.LogInformation($"api/v1/person GET request at {DateTime.Now:hh:mm:ss}");
+            try
             {
-                //TODO: add logging
-                return NotFound($"Not found Person {id}");
-            }
+                var person = await _context.Persons.Include(p => p.Skills).FirstOrDefaultAsync(x => x.Id == id);
 
-            //TODO: add logging
-            return Ok(person);
+                if (person == null)
+                {
+                    _logger.LogError($"Not found person {id}");
+                    return NotFound($"Not found Person {id}");
+                }
+
+                return Ok(person);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
+           
         }
 
         // PUT: api/v1/person/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutPerson(long id, Person person)
         {
+            _logger.LogInformation($"api/v1/person PUT request at {DateTime.Now:hh:mm:ss}");
+
             if (id != person.Id)
             {
-                //TODO: add logging
-                return BadRequest($"Not found Person {id}");
+                _logger.LogError($"BadRequest: person {id} not found");
+                return BadRequest("BadRequest");
             }
 
             _context.Entry(person).State = EntityState.Modified;
@@ -71,7 +95,7 @@ namespace HallOfFame.Controllers
             {
                 if (!PersonExists(id))
                 {
-                    //TODO: add logging
+                    _logger.LogError($"Not found Person {id}");
                     return NotFound($"Not found Person {id}");
                 }
                 else
@@ -79,7 +103,7 @@ namespace HallOfFame.Controllers
                     throw;
                 }
             }
-            //TODO: add logging
+
             return Ok("Data base updated!");
         }
 
@@ -87,6 +111,8 @@ namespace HallOfFame.Controllers
         [HttpPost]
         public async Task<ActionResult<Person>> PostPerson(Person person)
         {
+            _logger.LogInformation($"api/v1/person POST request at {DateTime.Now:hh:mm:ss}");
+
             _context.Persons.Add(person);
             await _context.SaveChangesAsync();
 
@@ -97,22 +123,33 @@ namespace HallOfFame.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeletePerson(long id)
         {
-            var person = await _context.Persons.Include(p => p.Skills).FirstOrDefaultAsync(x => x.Id == id);
-            if (person == null)
-            { 
-                //TODO: add logging
-                return NotFound($"Not found Person {id}");
-            }
+            _logger.LogInformation($"api/v1/person DELET request at {DateTime.Now:hh:mm:ss}");
 
-            _context.Persons.Remove(person);
-            await _context.SaveChangesAsync();
-            //TODO: add logging
-            return Ok($"Person {id} has been deleted");
+            try
+            {
+                var person = await _context.Persons.Include(p => p.Skills).FirstOrDefaultAsync(x => x.Id == id);
+                if (person == null)
+                {
+                    _logger.LogError($"Not found person {id}");
+                    return NotFound($"Not found Person {id}");
+                }
+
+                _context.Persons.Remove(person);
+                await _context.SaveChangesAsync();
+
+                return Ok($"Person {id} has been deleted");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
+            
         }
 
         private bool PersonExists(long id)
         {
-            return _context.Persons.Include(p=>p.Skills).Any(e => e.Id == id);
+            return _context.Persons.Include(p => p.Skills).Any(e => e.Id == id);
         }
     }
 }
