8c4609d4b998c59d51bdff2907c06cc0e6c4612e
jystlik <jystlik@gmail.com>
2021-05-23 11:55:00 +0700

    Создание интерфейса

---
+++ HallOfFame/Core/Interfaces/IPersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using HallOfFame.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HallOfFame.Core.Interfaces
+{
+    public interface IPersonRepository
+    {
+        Task<Person> GetByIdAsync(int id);
+        Task<List<Person>> ListAsync();
+        Task AddAsync(Person session);
+        Task UpdateAsync(Person session);
+    }
+}
