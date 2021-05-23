34ae6ffa2a87936476722d93bd71085ee073972e
jystlik <jystlik@gmail.com>
2021-05-23 11:40:17 +0700

    Revert "Добавление консольного логгирования"
    This reverts commit b62541fafad161ccbe177e2afe4031e345998d24.

--- HallOfFame/Startup.cs
+++ HallOfFame/Startup.cs
@@ -1,11 +1,16 @@
 using HallOfFame.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HallOfFame
 {
